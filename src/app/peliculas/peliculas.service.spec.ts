46f6b6aa4a8948c4aa2a9b44f278af4a71eb4e49
jortrifa <jorge.trigueros@datacronos.es>
2017-09-19 00:49:38 +0200

    Test unitarios Peliculas

--- src/app/peliculas/peliculas.service.spec.ts
+++ src/app/peliculas/peliculas.service.spec.ts
@@ -1,15 +1,88 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { TestBed, inject, fakeAsync, tick, async, getTestBed } from '@angular/core/testing';
+import {
+  Http,
+  ConnectionBackend,
+  BaseRequestOptions,
+  Response,
+  ResponseOptions,
+  RequestMethod,
+  HttpModule,
+  XHRBackend
+} from '@angular/http';
+import { MockBackend } from '@angular/http/testing';
 
 import { PeliculasService } from './peliculas.service';
+import { MockConnection } from '@angular/http/testing';
 
-describe('PeliculasService', () => {
+fdescribe('PeliculasService', () => {
+  // Arrange
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [PeliculasService]
+      providers: [
+        BaseRequestOptions,
+        MockBackend,
+        PeliculasService,
+        {
+          provide: Http,
+          deps: [MockBackend, BaseRequestOptions],
+          useFactory: (backend: ConnectionBackend, defaultOptions: BaseRequestOptions) => {
+            return new Http(backend, defaultOptions);
+          }
+        },
+      ]
     });
   });
 
-  it('should ...', inject([PeliculasService], (service: PeliculasService) => {
+  it('should be created', inject([PeliculasService], (service: PeliculasService) => {
     expect(service).toBeTruthy();
   }));
+
+  describe('test for getPeliculas', () => {
+
+    it('should return all peliculas',
+      inject([PeliculasService, MockBackend], fakeAsync((peliculasService, mockBackend) => {
+
+        // Arrange
+        let dataResponse, dataUrl, dataMethod;
+        const peliculasMock = {
+          results: [
+            {
+              id: 3,
+              original_title: 'Spiderman I',
+              title: 'Spiderman I',
+              overview: 'Lorem ipsum dolor sit amet',
+              backdrop_path: ''
+            },
+            {
+              id: 4,
+              original_title: 'Spiderman II',
+              title: 'Spiderman II',
+              overview: 'Lorem ipsum dolor sit amet',
+              backdrop_path: ''
+            }
+          ]
+        }
+        const mockResponse = new ResponseOptions({ body: JSON.stringify(peliculasMock) });
+        mockBackend.connections.subscribe(connection => {
+          dataUrl = connection.request.url;
+          dataMethod = connection.request.method;
+          connection.mockRespond(new Response(mockResponse));
+        });
+
+        // Act
+        peliculasService.getPeliculas()
+          .subscribe(response => {
+            dataResponse = response;
+          });
+        tick();
+        // Assert
+        expect(dataResponse.length).toEqual(2);
+        expect(dataMethod).toBe(RequestMethod.Get);
+        expect(dataResponse[0].id).toEqual(3);
+      }))
+    );
+
+  });
+
+
 });
